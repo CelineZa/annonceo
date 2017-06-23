d0ab77f5e3fd5fc5ea2dfc6265753b28b050689a
thimyxuan <thimyxuan.nguyen@gmail.com>
2017-06-23 17:28:04 +0200

    liens haut.inc

--- inc/haut.inc.php
+++ inc/haut.inc.php
@@ -26,7 +26,7 @@
 					?>
 			    	<ul class="nav navbar-nav">
 						<?php
-								echo '<li><a href="' . URL . 'admin/gestion_annonce.php">Annonces</a></li>';
+								echo '<li><a href="' . URL . 'admin/gestion_annonce.php?action=affichage">Annonces</a></li>';
 								echo '<li><a href="' . URL . 'admin/gestion_categorie.php">Catégories</a></li>';
 								echo '<li><a href="' . URL . 'admin/gestion_commentaire.php">Commentaires</a></li>';
 								echo '<li><a href="' . URL . 'admin/gestion_membre.php">Membres</a></li>';
@@ -44,7 +44,8 @@
 							if(internauteEstConnecte())
 							{
 								echo '<li><a href="' . URL . 'membre/profil.php">Mon profil</a></li>';
-								echo '<li><a href="' . URL . 'membre/ajouter_annonce.php">Ajouter une annonce</a></li>';
+								echo '<li><a href="' . URL . 'membre/gestion_annonce_membre.php?action=affichage">Mes annonces</a></li>';
+								echo '<li><a href="' . URL . 'membre/gestion_annonce_membre.php?action=ajout">Déposer une annonce</a></li>';
 							}
 							echo '<li><a href="' . URL . 'qui_sommes_nous.php">Qui Sommes Nous</a></li>';
 							echo '<li><a href="' . URL . 'contact.php">Contact</a></li>';
